e71b7e8bf9ff130059234514d4704dc9f1beb086
Petter-7 <psancardo907@gmail.com>
2026-05-20 19:47:07 -0600

    ej2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,25 +1,29 @@
 import pulp
 
-# 1. Definir el problema (Minimización)
-model = pulp.LpProblem("Cloud_Optimization", pulp.LpMinimize)
+# 1. Definir el problema (Maximización)
+model = pulp.LpProblem("Computer_Assembly_Optimization", pulp.LpMaximize)
 
-# 2. Definir Variables (Enteras, ya que no puedes rentar media instancia)
-x1 = pulp.LpVariable("Instancia_A", lowBound=0, upBound=40, cat='Integer')
-x2 = pulp.LpVariable("Instancia_B", lowBound=0, upBound=30, cat='Integer')
+# 2. Definir Variables
+# Son enteras porque no se pueden ensamblar medias computadoras
+x = pulp.LpVariable("Escritorios", lowBound=0, cat='Integer')
+y = pulp.LpVariable("Laptops", lowBound=0, cat='Integer')
 
 # 3. Función Objetivo
-model += 10 * x1 + 30 * x2, "Costo_Total"
+model += 2000 * x + 4000 * y, "Ganancia_Total"
 
 # 4. Restricciones
-model += 2 * x1 + 4 * x2 >= 100, "CPU_Demand"
-model += 1 * x1 + 5 * x2 >= 80, "RAM_Demand"
 
-# 5. Resolver y mostrar
+# Restricción de microprocesadores
+model += x + y <= 60, "Procesadores_Disponibles"
+
+# Restricción de horas de trabajo
+model += x + 3 * y <= 100, "Horas_Trabajo"
+
+# 5. Resolver el modelo
 model.solve()
 
+# 6. Mostrar resultados
 print(f"Estado: {pulp.LpStatus[model.status]}")
-print(f"Contratar Tipo A: {x1.varValue}")
-print(f"Contratar Tipo B: {x2.varValue}")
-print(f"Costo Mínimo Diario: ${pulp.value(model.objective)}")
-
-#source .venv/bin/activate
+print(f"Computadoras de Escritorio: {x.varValue}")
+print(f"Laptops: {y.varValue}")
+print(f"Ganancia Máxima: ${pulp.value(model.objective)} MXN")
